aa5c1626ee14d1b001ff98f000b351e9df1a2144
JUNYEONG-K <godoly1211@naver.com>
2023-03-05 00:16:50 +0900

    SOLVE: Two Pointers, Sliding Window - 공통원소 구하기

---
+++ src/two_pointers_sliding_window/CommonElement.java
@@ -0,0 +1,39 @@
+package two_pointers_sliding_window;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CommonElement {
+    static ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        // Two pointers -> 각 배열이 정렬되어 있어야 함.
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int p1 = 0, p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (a[p1] == b[p2]) {
+                answer.add(a[p1++]);
+                p2++;
+            }
+            else if (a[p1] < b[p2]) p1++;
+            else p2++;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int m = scanner.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = scanner.nextInt();
+        }
+        ArrayList<Integer> solution = solution(n, m, a, b);
+        System.out.println(solution);
+    }
+}
