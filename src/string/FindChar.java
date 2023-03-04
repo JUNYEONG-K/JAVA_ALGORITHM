da436a04785993d87372492b3b24c2920a95dda9
JUNYEONG-K <godoly1211@naver.com>
2023-03-04 12:23:49 +0900

    SOLVE: 문자열 - 문자찾기

---
+++ src/string/FindChar.java
@@ -0,0 +1,30 @@
+package string;
+
+import java.util.Scanner;
+
+public class FindChar {
+    public static int solution(String str, char target) {
+        int answer = 0;
+         str = str.toUpperCase();
+         target = Character.toUpperCase(target);
+
+//        for (int i = 0; i < str.length(); i++) {
+//            if (str.charAt(i) == target) answer++;
+//        }
+
+        for (char c : str.toCharArray()) {
+            if (c == target) answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        char target = scanner.next().charAt(0);
+
+        int result = solution(str, target);
+        System.out.println(result);
+    }
+}
