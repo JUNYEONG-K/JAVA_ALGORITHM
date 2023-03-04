5a80a35c66ea1c9e2bb2da0a7b74ed0b9e339b31
JUNYEONG-K <godoly1211@naver.com>
2023-03-04 22:44:07 +0900

    SOLVE: 배열 - 봉우리

---
+++ src/array/Peak.java
@@ -0,0 +1,39 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Peak {
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int solution(int n, int[][] ground) {
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean peak = true;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    // 조건문 순서
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n && ground[nx][ny] > ground[i][j]) {
+                        peak = false;
+                        break;
+                    }
+                }
+                if (peak) answer++;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] ground = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                ground[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println(solution(n, ground));
+    }
+}
