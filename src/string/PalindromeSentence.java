46c33dd6fd40ae419dc387b68965097300d5da8f
JUNYEONG-K <godoly1211@naver.com>
2023-03-04 14:46:01 +0900

    SOLVE: 문자열 - 팰린드롬

---
+++ src/string/PalindromeSentence.java
@@ -0,0 +1,18 @@
+package string;
+
+import java.util.Scanner;
+
+public class PalindromeSentence {
+    static String solution(String sentence) {
+        sentence = sentence.toLowerCase().replaceAll("[^a-z]", "");
+        String reversed = new StringBuilder(sentence).reverse().toString();
+        if (sentence.equals(reversed)) return "YES";
+        return "NO";
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String sentence = scanner.nextLine();
+        String result = solution(sentence);
+        System.out.println(result);
+    }
+}
