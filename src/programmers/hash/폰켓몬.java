181c4449ec56f37b296ade0b885c0c371f89d9e8
JUNYEONG-K <godoly1211@naver.com>
2023-03-08 23:48:08 +0900

    SOLVE: 프로그래머스 - 해시 - 폰켓몬

---
+++ src/programmers/hash/폰켓몬.java
@@ -0,0 +1,16 @@
+package programmers.hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class 폰켓몬 {
+    public int solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num: nums) {
+            set.add(num);
+        }
+//        return Math.min(set.size(), nums.length / 2);
+        return Integer.min(set.size(), nums.length / 2);
+
+    }
+}
